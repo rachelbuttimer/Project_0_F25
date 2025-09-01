4e10668e4990322b33cef9ad3f04537b7dd9d45e
Rachel Buttimer <rachel.buttimer@gmail.com>
2025-08-31 22:31:34 -0400

    added null terminator, header file

--- random.c
+++ random.c
@@ -1,7 +1,12 @@
 #include <string.h>
+#include <stdlib.h>
+#include "random.h"
 
-char* rand_string(char* s, int size){
+void rand_string(char* s, int size){
     for(int i = 0; i < size; i++){
         s[i] = 'a' + (rand() % 26);
     }
+
+    //null terminator
+    s[size] = '\0';
 }
---
+++ random.h
@@ -0,0 +1,6 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+void rand_string(char *, int);
+
+#endif
